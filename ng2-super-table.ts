512848edd3eaf4e0451cda926da9fe4e3e021fe6
Andrew Perlitch <perlitch@cisco.com>
2016-07-28 12:52:49 -0700

    restricting exported directives

--- ng2-super-table.ts
+++ ng2-super-table.ts
@@ -1,10 +1,11 @@
 import {SuperTable} from './src/superTable.component';
+export {SuperTable} from './src/superTable.component';
 export * from './src/interfaces';
 export {superTableSorters} from './src/superTableSorters';
 export {superTableFilters} from './src/superTableFilters';
 export {SuperTableCell} from './src/superTableCell.component';
 
-export * from './src/superTable.component';
+// export * from './src/superTable.component';
 
 // for angular-cli
 export default {
