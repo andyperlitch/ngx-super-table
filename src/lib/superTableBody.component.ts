andyperlitch/ngx-super-table

commit e6a48a9f03b243df7d5595e4ae74071ab99955c5

style: Cleanup coding style

src/lib/superTableBody.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input, ElementRef, SimpleChanges, OnChanges, OnInit, OnDestroy } from '@angular/core';
+import {
+ Component,
+ ElementRef,
+ HostListener,
+ Input,
+ OnChanges,
+ SimpleChanges
+} from '@angular/core';
 import { SuperTableState } from './SuperTableState';
 import { ISuperTableOptions } from './interfaces';
 import * as _ from 'lodash';
 
-const DEFAULT_ROW_HEIGHT : number = 20;
-const PADDING_ROW_COUNT : number = 20;
-const DEBOUNCE_DELAY : number = 250;
+const DEFAULT_ROW_HEIGHT = 20;
+const PADDING_ROW_COUNT = 20;
+const DEBOUNCE_DELAY = 250;
 
 @Component({
   selector: 'super-table-body',
@@ -35,9 +42,6 @@ const DEBOUNCE_DELAY : number = 250;
       </tbody>
     </table>
   `,
-  host: {
-    '(scroll)': 'trackScroll($event)'
-  },
   styles: [`
     :host {
       display: block;
@@ -57,43 +61,43 @@ const DEBOUNCE_DELAY : number = 250;
     }
   `]
 })
-export class SuperTableBody implements OnChanges, OnInit, OnDestroy {
-  @Input() rows : Array<any>;
-  @Input() tableClasses : any;
-  @Input() bodyHeight : number;
-  @Input() options : ISuperTableOptions;
+export class SuperTableBody implements OnChanges {
+  @Input() rows: Array<any>;
+  @Input() tableClasses: any;
+  @Input() bodyHeight: number;
+  @Input() options: ISuperTableOptions;
 
-  private visibleRows : Array<any> = [];
+  visibleRows: Array<any> = [];
 
   // assume small row height at first.
   // The real height will be detected once rows are rendered.
-  private rowHeight : number = DEFAULT_ROW_HEIGHT;
-  private rowOffset : number = 0;
-  private trackScroll : Function = _.debounce( () => {
-    this.updateVisibleRows();
-  }, DEBOUNCE_DELAY);
-  private onWindowResize : EventListener = _.debounce( () => {
-    this.detectRowHeight();
-    this.updateVisibleRows();
-  });
+  rowHeight: number = DEFAULT_ROW_HEIGHT;
+  rowOffset = 0;
 
-  constructor (private el: ElementRef, private state: SuperTableState) {}
+  constructor (private el: ElementRef, public state: SuperTableState) {}
 
-  ngOnInit () : void {
-    window.addEventListener('resize', this.onWindowResize);
-  }
-
-  ngOnDestroy () : void {
-    window.removeEventListener('resize', this.onWindowResize);
-  }
-
-  ngOnChanges (changes: SimpleChanges) : void {
+  ngOnChanges(changes: SimpleChanges): void {
     this.updateVisibleRows();
   }
 
-  private updateVisibleRows () : void {
-    let startIndex : number, endIndex : number;
-    let currentScroll : number = this.el.nativeElement.scrollTop;
+  @HostListener('scroll')
+  trackScroll() {
+    _.debounce( () => {
+      this.updateVisibleRows();
+    }, DEBOUNCE_DELAY);
+  }
+
+  @HostListener('resize')
+  onWindowResize() {
+    _.debounce( () => {
+      this.detectRowHeight();
+      this.updateVisibleRows();
+    });
+  }
+
+  private updateVisibleRows(): void {
+    let startIndex: number, endIndex: number;
+    const currentScroll: number = this.el.nativeElement.scrollTop;
 
     startIndex = Math.floor(currentScroll / this.rowHeight - PADDING_ROW_COUNT);
     startIndex = Math.max(0, startIndex);
@@ -108,8 +112,8 @@ export class SuperTableBody implements OnChanges, OnInit, OnDestroy {
 
   }
 
-  private detectRowHeight () : void {
-    let tr : HTMLTableSectionElement = this.el.nativeElement.querySelector('tbody.visible-rows tr');
+  private detectRowHeight(): void {
+    const tr: HTMLTableSectionElement = this.el.nativeElement.querySelector('tbody.visible-rows tr');
     if (tr != null) {
       this.rowHeight = tr.offsetHeight;
     }

src/app/instrument.component.ts
@@ -5,4 +5,6 @@ import { SuperTableCell } from '../lib';
   selector: 'app-instrument-cell',
   template: `<span class="badge badge-primary">{{ value }}</span>`
 })
-export class InstrumentComponent extends SuperTableCell { }
+export class InstrumentComponent extends SuperTableCell {
+  value: any;
+}

src/lib/SuperTableState.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject }    from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { ISuperTableColumn, ColumnState, SORT_ORDER } from './interfaces';
 
-const sortCycle : SORT_ORDER[] = ['ASC', 'DESC', null];
-const getNextSortOrder : Function = function(currentSortOrder : SORT_ORDER) : SORT_ORDER {
-  let nextIndex : number = (sortCycle.indexOf(currentSortOrder) + 1) % sortCycle.length;
+const sortCycle: SORT_ORDER[] = ['ASC', 'DESC', null];
+const getNextSortOrder: Function = function(currentSortOrder: SORT_ORDER): SORT_ORDER {
+  const nextIndex: number = (sortCycle.indexOf(currentSortOrder) + 1) % sortCycle.length;
   return sortCycle[nextIndex];
 };
 
@@ -13,19 +13,19 @@ const getNextSortOrder : Function = function(currentSortOrder : SORT_ORDER) : SO
 export class SuperTableState {
 
   // publicly exposed properties
-  hasAnyFilters : boolean = false;
-  columns : ColumnState[];
-  sortStack : ColumnState[] = [];
-  stateChanged$ : Observable<SuperTableState>;
+  hasAnyFilters = false;
+  columns: ColumnState[];
+  sortStack: ColumnState[] = [];
+  stateChanged$: Observable<SuperTableState>;
 
   // source of observable
-  private stateChangedSource : BehaviorSubject<SuperTableState> = new BehaviorSubject<SuperTableState>(this);
+  private stateChangedSource: BehaviorSubject<SuperTableState> = new BehaviorSubject<SuperTableState>(this);
 
   constructor() {
     this.stateChanged$ = this.stateChangedSource.asObservable();
   }
 
-  public setColumns ( columns : Array<ISuperTableColumn> ) : void {
+  public setColumns(columns: Array<ISuperTableColumn>): void {
     this.columns = columns.map(c => {
       if (!!c.filter) {
         this.hasAnyFilters = true;
@@ -43,14 +43,14 @@ export class SuperTableState {
     });
   }
 
-  public toggleSort ( colState : ColumnState, doNotClear : boolean ) : void {
+  toggleSort(colState: ColumnState, doNotClear: boolean): void {
 
     // Set next sort order
     colState.sortOrder = getNextSortOrder(colState.sortOrder);
 
     // Check if we are clearing the rest of the sort stack or not
     if (doNotClear) {
-      let curIndex : number = this.sortStack.indexOf(colState);
+      const curIndex: number = this.sortStack.indexOf(colState);
       if (curIndex === -1) {
         this.sortStack.push(colState);
       } else if (!colState.sortOrder) {
@@ -68,7 +68,7 @@ export class SuperTableState {
     this.notify();
   }
 
-  public notify () : void {
+  notify(): void {
     this.stateChangedSource.next(this);
   }
 

src/lib/dummyRows.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
-const BG_IMAGE_DATA : string = [
+const BG_IMAGE_DATA: string = [
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAlCAYAAACDKIOp',
   'AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAABtJREFUeNpiuH',
   'v37n+G////MzAxAMHQIQACDAC7twbaN2nkgwAAAABJRU5ErkJggg=='].join('');

src/lib/enumFilter.component.ts
@@ -67,24 +67,24 @@ export class EnumFilterDropdown implements OnInit, OnDestroy {
     private el: ElementRef
   ) {}
 
-  ngOnInit () : void {
-    let styles: CSSStyleDeclaration = this.el.nativeElement.style;
+  ngOnInit (): void {
+    const styles: CSSStyleDeclaration = this.el.nativeElement.style;
     styles.top = this.top + 'px';
     styles.left = this.left + 'px';
     styles.width = this.width + 'px';
   }
 
-  ngOnDestroy () : void {
+  ngOnDestroy (): void {
     // to ensure that references to parent component
     // do not prevent GC
     this.destroyMe = null;
   }
 
-  private onChoiceChange() : void {
+  onChoiceChange(): void {
     this.state.notify();
   }
 
-  private showAll() : void {
+  showAll(): void {
     _.forEach(this.column.filterValue, (val, key) => {
       this.column.filterValue[key] = true;
     });
@@ -142,8 +142,8 @@ export class EnumFilter implements OnInit, OnDestroy {
 
   private dropdown: ComponentRef<EnumFilterDropdown>;
   private disabledChoices: Set<any> = new Set<any>();
+  disabledFilterCount = 0;
   private subscription: Subscription;
-  private disabledFilterCount: number = 0;
 
   constructor(
     private state: SuperTableState,
@@ -152,7 +152,7 @@ export class EnumFilter implements OnInit, OnDestroy {
     private resolver: ComponentFactoryResolver
   ) {}
 
-  ngOnInit () : void {
+  ngOnInit (): void {
     // initialize filtered values to include all
     this.column.filterValue = {};
     this.column.def.filterChoices.forEach(choice => {
@@ -165,11 +165,11 @@ export class EnumFilter implements OnInit, OnDestroy {
     });
   }
 
-  ngOnDestroy () : void {
+  ngOnDestroy (): void {
     this.subscription.unsubscribe();
   }
 
-  private toggleVisibility() : void {
+  toggleVisibility(): void {
     if (this.dropdown) {
       this.dropdown.destroy();
       this.dropdown = null;

src/lib/interfaces.ts
@@ -1,32 +1,28 @@
 export type FILTER_TYPE = 'TEXT' | 'ENUM';
 export type SORT_ORDER = 'ASC' | 'DESC';
 
-export interface ISuperTableSorter {
-  (row1Value: any, row2Value: any, row1: Object, row2: Object): number;
-}
+export type ISuperTableSorter = (row1Value: any, row2Value: any, row1: Object, row2: Object) => number;
 
 export interface IBuiltInSorters {
   STRING: ISuperTableSorter;
   NUMBER: ISuperTableSorter;
 }
 
-export interface ISuperTableCellFormatter {
-  (value: any, row: Object, column: ColumnState): string;
-}
+export type ISuperTableCellFormatter = (value: any, row: Object, column: ColumnState) => string;
 
 export interface ISuperTableOptions {
   autoHeight?: boolean;
 }
 
 export interface ColumnState {
-  id : string;
-  filterValue : any;
-  sortOrder : SORT_ORDER;
-  isHidden : boolean;
-  width : number;
-  def : ISuperTableColumn;
-  hasSort : boolean;
-  hasFilter : boolean;
+  id: string;
+  filterValue: any;
+  sortOrder: SORT_ORDER;
+  isHidden: boolean;
+  width: number;
+  def: ISuperTableColumn;
+  hasSort: boolean;
+  hasFilter: boolean;
 }
 
 export interface ISuperTableFilter {
@@ -43,11 +39,11 @@ export interface ISuperTableFilter {
 }
 
 export interface ISuperTableColumn {
-  id : string;
-  key : string;
-  label : string;
-  width? : number;
-  lockWidth? : boolean;
+  id: string;
+  key: string;
+  label: string;
+  width?: number;
+  lockWidth?: boolean;
   sort?: ISuperTableSorter;
   filter?: ISuperTableFilter;
   format?: ISuperTableCellFormatter;

src/lib/superTable.component.ts
@@ -1,17 +1,17 @@
 import {
-  Component,
-  Input,
-  OnInit,
-  AfterViewInit,
   AfterContentInit,
+  AfterViewInit,
+  Component,
   ElementRef,
+  Input,
   OnChanges,
-  SimpleChanges,
-  OnDestroy
+  OnDestroy,
+  OnInit,
+  SimpleChanges
 } from '@angular/core';
 import { ISuperTableColumn, ColumnState, ISuperTableOptions } from './interfaces';
 import { SuperTableState } from './SuperTableState';
-import { Subscription }   from 'rxjs/Subscription';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'super-table',
@@ -55,27 +55,27 @@ export class SuperTable implements AfterContentInit, OnChanges, OnDestroy, OnIni
   @Input() tableClasses: any;
 
   // properties
-  private isReady : boolean = false;
-  private hasError : boolean = false;
-  private bodyHeight : number;
-  private filteredSortedRows : Array<any>;
-  private subscription : Subscription;
+  isReady = false;
+  hasError = false;
+  bodyHeight: number;
+  filteredSortedRows: Array<any>;
+  private subscription: Subscription;
 
   constructor (private el: ElementRef, private state: SuperTableState) {}
 
-  ngOnInit () : void {
+  ngOnInit(): void {
     this.subscription = this.state.stateChanged$.subscribe(() => this.sortAndFilterRows());
   }
 
-  ngAfterContentInit () : void {
+  ngAfterContentInit(): void {
     if (this.options.autoHeight) {
-      let parentHeight : number = this.el.nativeElement.parentElement.clientHeight;
+      const parentHeight: number = this.el.nativeElement.parentElement.clientHeight;
       this.setTableHeight(parentHeight);
     }
     this.isReady = true;
   }
 
-  ngOnChanges (changes: SimpleChanges) : void {
+  ngOnChanges(changes: SimpleChanges): void {
     // Inform state of columns changes
     if (changes['columns'].isFirstChange()) {
       this.state.setColumns(changes['columns'].currentValue);
@@ -83,29 +83,29 @@ export class SuperTable implements AfterContentInit, OnChanges, OnDestroy, OnIni
     this.sortAndFilterRows();
   }
 
-  ngOnDestroy () : void {
+  ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  private setTableHeight (totalHeight: number) : void {
+  private setTableHeight(totalHeight: number): void {
     // calculate header height
-    let headerHeight : number = this.el.nativeElement.querySelector('super-table-head').offsetHeight;
+    const headerHeight: number = this.el.nativeElement.querySelector('super-table-head').offsetHeight;
     // subtract it from totalHeight, set bodyHeight to result
     this.bodyHeight = totalHeight - headerHeight;
   }
 
-  private sortAndFilterRows () : void {
+  private sortAndFilterRows(): void {
     // Filtering
-    let activeFilterColumns : ColumnState[] = this.state.columns.filter((c) => {
+    const activeFilterColumns: ColumnState[] = this.state.columns.filter((c) => {
       return !!c.def.filter && !!c.def.filter.isActive(c.filterValue);
     });
 
-    if ( activeFilterColumns.length ) {
+    if (activeFilterColumns.length) {
       this.filteredSortedRows = this.rows.filter((row) => {
-        for (let i: number = 0; i < activeFilterColumns.length; i++) {
-          let colState : ColumnState = activeFilterColumns[i];
-          let val : any = row[colState.def.key];
-          let filterResult : boolean = colState.def.filter.fn(colState.filterValue, val, row);
+        for (let i = 0; i < activeFilterColumns.length; i++) {
+          const colState: ColumnState = activeFilterColumns[i];
+          const val: any = row[colState.def.key];
+          const filterResult: boolean = colState.def.filter.fn(colState.filterValue, val, row);
           if (filterResult === false) {
             return false;
           }
@@ -119,10 +119,10 @@ export class SuperTable implements AfterContentInit, OnChanges, OnDestroy, OnIni
     // Sorting
     this.filteredSortedRows.sort( (a, b) => {
       for (let i = 0; i < this.state.sortStack.length; i++) {
-        let colState : ColumnState = this.state.sortStack[i];
-        let val1 = a[colState.def.key];
-        let val2 = b[colState.def.key];
-        let compareResult : number = colState.sortOrder === 'ASC'
+        const colState: ColumnState = this.state.sortStack[i];
+        const val1 = a[colState.def.key];
+        const val2 = b[colState.def.key];
+        const compareResult: number = colState.sortOrder === 'ASC'
           ? colState.def.sort(val1, val2, a, b)
           : colState.def.sort(val2, val1, b, a);
         if (compareResult !== 0) {

src/lib/superTableFilters.ts
@@ -18,20 +18,20 @@ unitmap['month']                            = unitmap['week'] * 4;
 unitmap['year'] = unitmap['yr'] = unitmap['y']    = unitmap['day'] * 365;
 
 const clauseExp: RegExp = /(\d+(?:\.\d+)?)\s*([a-z]+)/;
-function parseDateFilter(str: string) : number {
+function parseDateFilter(str: string): number {
   // split on clauses (if any)
-  let clauses: string[] = str.trim().split(',');
-  let total: number = 0;
+  const clauses: string[] = str.trim().split(',');
+  let total = 0;
 
   // parse each clause
-  for (let i: number = 0; i < clauses.length; i++) {
-    let clause: string = clauses[i].trim();
-    let terms: RegExpExecArray = clauseExp.exec(clause);
+  for (let i = 0; i < clauses.length; i++) {
+    const clause: string = clauses[i].trim();
+    const terms: RegExpExecArray = clauseExp.exec(clause);
     if (!terms) {
       continue;
     }
-    let count: number = parseFloat(terms[1]);
-    let unit: string = terms[2].replace(/s$/, '');
+    const count: number = parseFloat(terms[1]);
+    const unit: string = terms[2].replace(/s$/, '');
     if (! unitmap.hasOwnProperty(unit) ) {
       continue;
     }
@@ -45,14 +45,14 @@ export const superTableFilters: IBuiltInFilters = {
     title: 'Search by text, eg. "foo". Use "!" to ' +
            'exclude and "=" to match exact text, e.g. ' +
            '"!bar" or "=baz".',
-    isActive: function(filterValue: any) : boolean {
+    isActive: function(filterValue: any): boolean {
       return !!filterValue && !!(filterValue as string).trim();
     },
     placeholder: 'string search',
-    fn: function(term: string, value: any, row: Object) : boolean {
+    fn: function(term: string, value: any, row: Object): boolean {
       term = term.toLowerCase().trim();
       value = String(value).toLowerCase();
-      let first: string = term[0];
+      const first: string = term[0];
 
       // negate
       if (first === '!') {
@@ -82,17 +82,17 @@ export const superTableFilters: IBuiltInFilters = {
     title: 'Search by number, e.g. "123". Optionally use comparator ' +
            'expressions like ">=10" or "<1000". Use "~" for approx. ' +
            'int values, eg. "~3" will match "3.2"',
-    isActive: function(filterValue: any) : boolean {
+    isActive: function(filterValue: any): boolean {
       return !!filterValue && !!(filterValue as string).trim();
     },
     placeholder: 'number search',
-    fn: function(term: string, value: any, row: Object) : boolean {
-      let parsedValue : number = parseFloat(value);
+    fn: function(term: string, value: any, row: Object): boolean {
+      const parsedValue: number = parseFloat(value);
       term = term.trim();
-      let firstTwo : string = term.substr(0, 2);
-      let firstChar : string = term[0];
-      let against1 : number = parseFloat(term.substr(1));
-      let against2 : number = parseFloat(term.substr(2));
+      const firstTwo: string = term.substr(0, 2);
+      const firstChar: string = term[0];
+      const against1: number = parseFloat(term.substr(1));
+      const against2: number = parseFloat(term.substr(2));
       if ( firstTwo === '<=' ) {
         return parsedValue <= against2 ;
       }
@@ -119,11 +119,11 @@ export const superTableFilters: IBuiltInFilters = {
     title: 'Search by date. Enter a date string (RFC2822 or ' +
            'ISO 8601 date). You can also type "today", "yesterday", ' +
            '"> 2 days ago", "< 1 day 2 hours ago", etc.',
-    isActive: function(filterValue: any) : boolean {
+    isActive: function(filterValue: any): boolean {
       return !!filterValue && !!(filterValue as string).trim();
     },
     placeholder: 'date search',
-    fn: function(term: string, value: Date | number) : boolean{
+    fn: function(term: string, value: Date | number): boolean{
       // today
       // yesterday
       // 1 day ago
@@ -140,11 +140,11 @@ export const superTableFilters: IBuiltInFilters = {
       if (!term) {
         return true;
       }
-      let numValue: number = value.valueOf();
-      let nowDate: Date = new Date();
-      let now: number = nowDate.valueOf();
-      let first_char: string = term[0];
-      let other_chars: string = (term.substr(1)).trim();
+      const numValue: number = value.valueOf();
+      const nowDate: Date = new Date();
+      const now: number = nowDate.valueOf();
+      const first_char: string = term[0];
+      const other_chars: string = (term.substr(1)).trim();
       let lowerbound: number, upperbound: number;
       if ( first_char === '<' ) {
         lowerbound = now - parseDateFilter(other_chars);
@@ -163,7 +163,7 @@ export const superTableFilters: IBuiltInFilters = {
         return new Date(numValue).toDateString() === new Date(now - unitmap['d']).toDateString();
       }
 
-      let supposedDate: Date = new Date(term);
+      const supposedDate: Date = new Date(term);
       if (!isNaN(supposedDate.valueOf())) {
         return new Date(numValue).toDateString() === supposedDate.toDateString();
       }
@@ -175,17 +175,17 @@ export const superTableFilters: IBuiltInFilters = {
     type: 'ENUM',
     title: 'Click to filter rows by discreet values',
     placeholder: 'filters',
-    isActive: function(filterValue: any) : boolean {
+    isActive: function(filterValue: any): boolean {
       if (!filterValue) {
         return false;
       }
-      let filterValueObj: Object = filterValue as Object;
+      const filterValueObj: Object = filterValue as Object;
       return Object.keys( filterValueObj ).some(key => {
         return !filterValueObj[key];
       });
     },
-    fn: function(filterValue: any, value: any, row: Object) : boolean {
-      let filterValueObj: Object = filterValue as Object;
+    fn: function(filterValue: any, value: any, row: Object): boolean {
+      const filterValueObj: Object = filterValue as Object;
       return filterValueObj[value];
     }
   }

commit 7a4e33963c8295d2c6b24b85483d320011809552

fix(row-virt): Fix dummy row loading broken in e6a48a9

src/lib/superTableBody.component.ts
@@ -74,7 +74,16 @@ export class SuperTableBody implements OnChanges {
   rowHeight: number = DEFAULT_ROW_HEIGHT;
   rowOffset = 0;
 
-  constructor (private el: ElementRef, public state: SuperTableState) {}
+  private scrollHandler = _.debounce(() => {
+    this.updateVisibleRows();
+  }, DEBOUNCE_DELAY);
+
+  private resizeHandler = _.debounce(() => {
+    this.detectRowHeight();
+    this.updateVisibleRows();
+  });
+
+  constructor (private el: ElementRef, public state: SuperTableState) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.updateVisibleRows();
@@ -82,17 +91,12 @@ export class SuperTableBody implements OnChanges {
 
   @HostListener('scroll')
   trackScroll() {
-    _.debounce( () => {
-      this.updateVisibleRows();
-    }, DEBOUNCE_DELAY);
+    this.scrollHandler();
   }
 
   @HostListener('resize')
   onWindowResize() {
-    _.debounce( () => {
-      this.detectRowHeight();
-      this.updateVisibleRows();
-    });
+    this.resizeHandler();
   }
 
   private updateVisibleRows(): void {